287ac80da7fcad4bec3c84a947221160a27f932e
dongkeonkim <dongkeonkim0810@gmail.com>
2024-08-31 20:34:13 +0900

    fix: 로그인한 유저가 /login 접근 불가능하게 수정

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { LoginContext } from '../contexts/LoginContextProvider';
 import LoginForm from '../components/Login/LoginForm';
 
+function LoginRedirect() {
+  const { isLogin } = useContext(LoginContext);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    console.log(isLogin);
+    if (isLogin) {
+      navigate('/'); // 로그인한 상태라면 홈으로 리다이렉트
+    }
+  }, [isLogin]);
+}
+
 const Login = () => {
+  LoginRedirect();
   return (
     <>
       <div>
