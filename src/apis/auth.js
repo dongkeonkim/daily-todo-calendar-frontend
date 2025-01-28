bc30657836da8fdf4f162efe0146444af4456ba8
dongkeonkim <dongkeonkim0810@gmail.com>
2025-01-28 22:09:57 +0900

    feat: 경로 카멜 -> 스네이크 수정

--- src/apis/auth.js
+++ src/apis/auth.js
@@ -8,7 +8,7 @@ export const login = (email, password) =>
 
 export const info = () => api.get(`/member/info`);
 
-export const join = (data) => api.post('/auth/signUp', data);
+export const join = (data) => api.post('/auth/sign-up', data);
 
 export const update = (data) => api.put('/member/update', data);
 
